4dde11a2ed7dcaa2de0f267e53507428e178fa8a
AiselUSMANOVA <43522498+AiselUSMANOVA@users.noreply.github.com>
2018-10-25 23:51:48 +0600

    Update find_max.py

--- baby_steps/find_max.py
+++ baby_steps/find_max.py
@@ -1,51 +1,88 @@
-"""
-Every function has documentation (docstring) which describes it's functionality and your goal is to
-implement all of them.
-
-Good Luck!
-"""
-
-
 def get_max(a, b):
     """
-    Returns max number among a and b.
+       Compare two values and return max value
+    :param a: int
+    :param b: int
+    :return: max value
     """
+    if a>b:
+        return a
+    else:
+        b
 
 
 def get_max_without_arguments():
     """
-    Raise TypeError exception with message as an argument.
+       Raise Error, if there is no argument
     """
+    raise TypeError("No arguments");
 
 
 def get_max_with_one_argument(a):
     """
-    Returns that value.
+       :param a: int
+       return that value
     """
+    return a
 
 
 def get_max_with_many_arguments(*args):
     """
-    Return the largest number among args.
+        return largest number among args
     """
+
+    max_number = float('-inf')
+
+    for arg in args:
+        if arg > max_number:
+            max_number = arg
+
+    return max_number
 
 
 def get_max_with_one_or_more_arguments(first, *args):
     """
-    Returns the largest number among first and args.
+        return largest number among first + args
     """
+
+    max_number = float('-inf')
+
+    for arg in (first, *args):
+        if arg > max_number:
+            max_number = arg
+
+    return max_number
 
 
 def get_max_bounded(*args, low, high):
     """
-    Returns the largest number among args bounded by low & high.
+        return largest number among args bounded by low & high
     """
+
+    max_number = float('-inf')
+
+    for arg in args:
+        if arg > max_number and (low < arg and arg < high):
+            max_number = arg
+
+    return max_number
 
 
 def make_max(*, low, high):
     """
-    Returns an inner function object which takes at last one argument
-    and return largest number amount it's arguments, but if the
-    largest number is larger than the 'high' which given as required
-    argument the inner function has to return it.
+        return inner function object which takes at last one argument
+        and return largest number amount it's arguments, but if the
+        largest number is larger than the 'high' which given as required
+        argument the inner function has to return it.
     """
+
+    def inner(first, *args):
+        max_number = float('-inf')
+
+        for arg in (first, *args):
+            if arg > max_number and (arg > low and arg < high):
+                max_number = arg
+                
+        return max_number
+
+    return inner
